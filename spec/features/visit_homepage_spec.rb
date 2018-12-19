acaf994e25ce5626d6b44a67055e71927060a2c3
Pauline <pbaker@hotmail.co.uk>
2018-12-19 15:31:00 +0000

    homepage has form and submit button
    Co-authored-by: CKKH 19429406+CKKH@users.noreply.github.com

--- spec/features/visit_homepage_spec.rb
+++ spec/features/visit_homepage_spec.rb
@@ -3,4 +3,10 @@ feature 'homepage' do
     visit '/'
     expect(page).to have_content('Welcome to our message app!')
   end
+
+  scenario 'will display a form' do
+    visit '/'
+    expect(page).to have_field("message")
+    click_button "Submit"
+  end
 end
