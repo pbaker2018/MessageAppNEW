43329f3912de78718a657521af0ff806496444c4
Pauline <pbaker@hotmail.co.uk>
2019-01-06 11:33:01 +0000

    added two unit tests. All tests passing. 100% coverage.

--- spec/unit/message_spec.rb
+++ spec/unit/message_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Message do
     Timecop.freeze(Time.now)
   end
 
-  subject (:message) { described_class.create(content: 'foo') }
+  subject (:message) { described_class.create(content: 'foo', name: 'Viv') }
 
   describe '#content' do
     it 'will display a user message' do
@@ -17,6 +17,18 @@ RSpec.describe Message do
   describe '#created_at' do
     it 'displays time that user submitted message' do
       expect(message.created_at).to eq Time.now
+    end
+  end
+
+  describe '#updated_at' do
+    it 'displays time that user edited message' do
+      expect(message.updated_at).to eq Time.now
+    end
+  end
+
+  describe '#name' do
+    it 'displays the name the user submitted' do
+      expect(message.name).to eq 'Viv'
     end
   end
 end
